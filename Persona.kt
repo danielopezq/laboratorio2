16576a910dfbb7c05d819ea0f716f34d915b5c17
Daniel López <128827756+danielopezq@users.noreply.github.com>
2026-03-26 14:29:05 -0600

    Agregando archivos

---
+++ Persona.kt
@@ -0,0 +1,12 @@
+package com.daniel.laboratorio2
+
+import androidx.compose.runtime.Composable
+
+class Persona (
+    val nombre: String,
+    val posicion: Int,
+    val listaPersonas: List <String>
+)
+
+
+
